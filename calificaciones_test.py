7962203b9a6eace39538eb88171b02ba1df20125
nombre_usuario <AS>
2025-10-01 19:24:34 +0200

    ala

---
+++ calificaciones_test.py
@@ -0,0 +1,3 @@
+from calificaciones import *
+if __name__ == "__main__":
+    print(nota_teoria(8.1,7.3))
---
+++ src/calificaciones.py
@@ -0,0 +1,9 @@
+def nota_teoria(nota_1,nota_2):
+    if nota_1  == None:
+        nota_1=0
+    if nota_2 == None:
+        nota_2=0
+        
+    res = (nota_1 + nota_2)/2
+    return res
+    
